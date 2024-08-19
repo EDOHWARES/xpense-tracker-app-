4aabf723fdbf4fdc81fbdd39a67340adeccd81be
EDOHWARES <edohemmanuel4real@gmail.com>
2024-08-19 23:00:19 +0100

    first conclude - auth

--- client/src/pages/auth/Index.jsx
+++ client/src/pages/auth/Index.jsx
@@ -1,14 +1,37 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import man from "../../assets/man.png";
 
 const Auth = () => {
-  const { loginWithRedirect, isAuthenticated } = useAuth0();
+  const { loginWithRedirect } = useAuth0();
 
   return (
-    <div>
-      <h1>Login in to Xpenser Tracker App</h1>
-        {
-            !isAuthenticated && <button onClick={() => loginWithRedirect()}>Sign In</button>
-        }
+    <div className="w-full h-screen max-h-screen p-4 flex items-center justify-center bg-[#EEF8F7]">
+      <div className="flex flex-col md:flex-row items-center justify-center text-center md:text-start w-full h-full max-w-[1440px]">
+        <div className="h-[25rem] md:h-full">
+          <img src={man} alt="hero-img" className="h-full" />
+        </div>
+        <div className="flex flex-col items-center">
+          <h1 className="font-bold text-[36px] text-[#438883] leading-[38px] mb-[1rem]">
+            Track Your Expenses <br />
+            Track Your Life
+          </h1>
+          <button
+            className="bg-[#3E7C78] text-white w-[356px] h-[67px] rounded-full font-semibold text-[18px] shadow-2xl"
+            onClick={() => loginWithRedirect()}
+          >
+            Get Started
+          </button>
+          <small className="text-[14px] text-[#444444] font-medium mt-[.5rem]">
+            Do Not Have An Account? 
+            <span
+              onClick={() => loginWithRedirect()}
+              className="text-[#438883] cursor-pointer"
+            >
+            . Sign Up
+            </span>
+          </small>
+        </div>
+      </div>
     </div>
   );
 };
--- client/src/pages/dashboard/Index.jsx
+++ client/src/pages/dashboard/Index.jsx
@@ -1,20 +1,42 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { Navigate } from "react-router-dom";
+import { ThreeDots } from "react-loader-spinner";
 
 const Dashboard = () => {
-  const { logout, isAuthenticated } = useAuth0();
+  const { logout, isLoading, error, isAuthenticated } = useAuth0();
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  if (isLoading) {
+    return (
+      <div className="w-full h-screen flex items-center justify-center">
+        <ThreeDots
+          visible={true}
+          height="80"
+          width="80"
+          color="#438883"
+          radius="9"
+          ariaLabel="three-dots-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+        />
+      </div>
+    );
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to={"/auth"} />;
+  }
+
   return (
-    <div>
-      <h1>Dashboard</h1>
-      {isAuthenticated && (
-        <button
-          onClick={() =>
-            logout()
-          }
-        >
-          Sign Out
-        </button>
-      )}
-    </div>
+    <>
+      <div>
+        <h1 className="text-3xl">Dashboard</h1>
+        {isAuthenticated && <button onClick={() => logout()}>Sign Out</button>}
+      </div>
+    </>
   );
 };
 
